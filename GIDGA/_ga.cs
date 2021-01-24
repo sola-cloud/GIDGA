23976c4cf08466d75ff235777316edb5cd1a4c40
orione moray <orionemoray@gmail.com>
2021-01-24 04:18:17 -0800

    Add project files.

---
+++ GIDGA/_ga.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GIDGA
+{
+    public class _ga
+    {
+        public string version { get; set; }
+        public string root { get; set; }
+        public string GenGa
+        {
+            get { return $"GA{version}.{root}.{uid()}.{timestamp()}"; }   
+        }
+        private static string timestamp()
+        {
+            return DateTimeOffset.Now.ToUnixTimeSeconds().ToString();
+        }
+        private static string uid()
+        {
+            var unqId = string.Empty;
+
+            while (unqId.Length < 10)
+                unqId += new Random().Next(1, 9).ToString();
+            return unqId;
+        }
+    }
+}
---
+++ GIDGA/_gid.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GIDGA
+{
+    public class _gid
+    {
+    }
+}
